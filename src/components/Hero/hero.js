4e644044c04a0345a75c1123c65ae07eab1dc67c
David Yang <dy46@duke.edu>
2016-08-31 14:04:56 -0400

    added hero

---
+++ src/components/Hero/hero.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import BackgroundImage from '../assets/hackduke-background.png'
+import classes from './Hero.scss'
+
+export const Hero = () => (
+  <div className={classes.hero}>
+    <div className={classes.mainText}>
+      <h1>HACK</h1>
+      <h1>DUKE</h1>
+      <h5>CODE FOR GOOD</h5>
+      <h6>November 13-14, 2016</h6>
+      <button>Apply Now</button>
+    </div>
+  </div>
+)
+
+export default Hero
