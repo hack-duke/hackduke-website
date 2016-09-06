2f956705328df5a5a13d0aa6fb358a0b96b35546
Briguy52 <Briguy52@gmail.com>
2016-09-06 16:20:47 -0400

    Extracted Sponsors About to constant array

--- src/components/SponsorsPage/SponsorsPage.js
+++ src/components/SponsorsPage/SponsorsPage.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import classes from './SponsorsPage.scss'
 
+const aboutText= ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
+                  "do eiusmod tempor incididunt ut labore et dolore magna aliqua"];
+
 export const SponsorsPage = () => (
   <div className={classes.sponsors}>
     <div className={'container text-center'}>
       <h1 className={classes.header}>SPONSORS</h1>
       <p className={classes.tracksText}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+        {aboutText[0]} <br/>
+        {aboutText[1]}
       </p>
     </div>
   </div>
