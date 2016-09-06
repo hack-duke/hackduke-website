c27eb4d910f17a80e71ac0ad2aadb4df0b0f3063
Briguy52 <Briguy52@gmail.com>
2016-09-06 16:21:27 -0400

    Extracted title to constant

--- src/components/SponsorsPage/SponsorsPage.js
+++ src/components/SponsorsPage/SponsorsPage.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import classes from './SponsorsPage.scss'
 
+const title="SPONSORS";
 const aboutText= ["Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed",
                   "do eiusmod tempor incididunt ut labore et dolore magna aliqua"];
 
 export const SponsorsPage = () => (
   <div className={classes.sponsors}>
     <div className={'container text-center'}>
-      <h1 className={classes.header}>SPONSORS</h1>
+      <h1 className={classes.header}>{title}</h1>
       <p className={classes.tracksText}>
         {aboutText[0]} <br/>
         {aboutText[1]}
