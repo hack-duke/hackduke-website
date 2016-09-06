c7686ffa27c0b57eefa66b367f05908fec8361e5
Andy <ownz.andy@gmail.com>
2016-09-06 17:41:26 -0400

    populating sponsor images

--- src/components/SponsorsPage/SponsorsPage.js
+++ src/components/SponsorsPage/SponsorsPage.js
@@ -1,5 +1,25 @@
 import React from 'react'
 import classes from '../About/About.scss'
+import SponsorsItem from 'components/SponsorsItem'
+import { Row, Col } from 'react-bootstrap'
+import a16z from 'static/sponsors/a16z.png'
+import aboutme from 'static/sponsors/aboutme.png'
+import akamai from 'static/sponsors/akamai.png'
+import appia from 'static/sponsors/appia.png'
+import apt from 'static/sponsors/apt.png'
+import att from 'static/sponsors/att-old.png'
+import au from 'static/sponsors/au.png'
+import aws from 'static/sponsors/aws.png'
+import bronto from 'static/sponsors/bronto.png'
+
+const sponsors_urls = ["http://google.com", "http://google.com", "http://google.com", "http://google.com", "http://google.com", "http://google.com", "http://google.com", "http://google.com", "http://google.com"]
+const image_urls = [a16z, aboutme, akamai, appia, apt, att, au, aws, bronto]
+
+const sponsorsItemAtIndex = (url, index, mod) => {
+  if(index % 3 === mod) {
+    return <SponsorsItem key={index} imageUrl={url} sponsorUrl={sponsors_urls[index]}/>
+  } 
+} 
 
 export const SponsorsPage = () => (
   <div>
@@ -8,6 +28,26 @@ export const SponsorsPage = () => (
       <p className={classes.tracksText}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
       </p>
+      <Row>
+        <Col md={4}>
+            {image_urls.map((url, index) =>  {
+                return sponsorsItemAtIndex(url, index, 0)
+              }
+            )}
+        </Col>
+        <Col md={4}>
+          {image_urls.map((url, index) =>  {
+              return sponsorsItemAtIndex(url, index, 1)
+            }
+          )}
+        </Col>
+        <Col md={4}>
+          {image_urls.map((url, index) =>  {
+              return sponsorsItemAtIndex(url, index, 2)
+            }
+          )}
+        </Col>
+      </Row>
     </div>
   </div>
 )
